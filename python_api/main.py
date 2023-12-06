4c5295cc67a452b41946f37077eed9807e53a2e9
nathan <nathan.poncet.pro@gmail.com>
2023-12-06 10:06:35 +0100

    🎉 feat(main.py): add Python API server implementation
    📝 Add a new file `main.py` to implement a Python API server. The server listens on `127.0.0.1:8000` and handles the following routes:
    - `/health`: Returns a 200 response with an empty body to indicate the server is healthy.
    - `/get`: Returns the current value of an iterator stored in a MySQL database.
    - `/add`: Accepts a JSON payload with a `value` field and updates the iterator value in the database.
    
    The server retrieves the database connection information from environment variables (`DB_HOST`, `DB_PORT`, `DB_USER`, `DB_PASSWORD`, `DB_DATABASE`). If these variables are not set, default values are used.
    
    The server uses the `mysql.connector` library to connect to the MySQL database and perform queries. It defines a `DatabaseHandler` class with two methods:
    - `get_iterator_value()`: Retrieves the current value of the iterator from the database.
    - `update_iterator_value(value)`: Inserts a new row with the specified value into the database.
    
    The server uses the `http.server` module to handle HTTP requests. It defines a `RequestHandler` class that extends `BaseHTTPRequestHandler` and overrides the `do_GET` and `do_POST` methods to handle the corresponding HTTP methods and routes.
    
    When a GET request is made to `/health`, the server returns a 200 response with an empty body.
    
    When a GET request is made to `/get`, the server retrieves the current iterator value from the database and returns it as a response.
    
    When a POST request is made to `/add`, the server expects a JSON payload with a `value` field. If the `value` is an integer, the server updates the iterator value in the database and returns a 200 response with a success message. If the `value` is not an integer or the payload is in an invalid format, the server returns a 400 response with an error message.
    
    For any other route, the server returns a 404 response with an error message.
    
    The server starts listening for incoming requests by calling the `run()` function.

---
+++ python_api/main.py
@@ -0,0 +1,101 @@
+import os
+import mysql.connector
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+
+# Récupérer les informations de connexion à la base de données depuis les variables d'environnement
+db_host = os.environ.get('DB_HOST', 'localhost')
+db_port = os.environ.get('DB_PORT', '3306')
+db_user = os.environ.get('DB_USER', 'root')
+db_password = os.environ.get('DB_PASSWORD', 'password')
+db_database = os.environ.get('DB_DATABASE', 'iterator-db')
+
+class DatabaseHandler:
+    def get_iterator_value():
+        try:
+            conn = mysql.connector.connect(
+                host=db_host,
+                port=db_port,
+                user=db_user,
+                password=db_password,
+                database=db_database
+            )
+            cursor = conn.cursor()
+            cursor.execute('SELECT state FROM interator ORDER BY id DESC LIMIT 1')
+            result = cursor.fetchone()
+            conn.close()
+            return result[0] if result else 0
+        except mysql.connector.Error as err:
+            print(f"Erreur MySQL : {err}")
+            return 0
+
+    def update_iterator_value(value):
+        try:
+            conn = mysql.connector.connect(
+                host=db_host,
+                port=db_port,
+                user=db_user,
+                password=db_password,
+                database=db_database
+            )
+            cursor = conn.cursor()
+            cursor.execute('INSERT INTO interator (state) VALUES (%s)', (value,))
+            conn.commit()
+            conn.close()
+        except mysql.connector.Error as err:
+            print(f"Erreur MySQL : {err}")
+
+class RequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/health':
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            response = ""
+        elif self.path == '/get':
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            iterator_value = DatabaseHandler.get_iterator_value()
+            response = 'iterator: ' + str(iterator_value)
+        else:
+            self.send_response(404)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            response = 'Route not found'
+        
+        self.wfile.write(response.encode('utf-8'))
+
+    def do_POST(self):
+        if self.path == '/add':
+            content_length = int(self.headers['Content-Length'])
+            post_data = self.rfile.read(content_length)
+            try:
+                data = json.loads(post_data.decode('utf-8'))
+                value = data['value']
+                if isinstance(value, int):
+                    DatabaseHandler.update_iterator_value(value)
+                    self.send_response(200)
+                    response = 'Value updated successfully'
+                else:
+                    self.send_response(400)
+                    response = 'Invalid input value (not an integer)'
+            except (KeyError, json.JSONDecodeError):
+                self.send_response(400)
+                response = 'Invalid input format'
+        else:
+            self.send_response(404)
+            response = 'Route not found'
+        
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        self.wfile.write(response.encode('utf-8'))
+
+def run():
+    server_address = ('127.0.0.1', 8000)
+    httpd = HTTPServer(server_address, RequestHandler)
+    print('Serveur HTTP démarré sur 127.0.0.1:8000...')
+    httpd.serve_forever()
+
+if __name__ == '__main__':
+    run()
