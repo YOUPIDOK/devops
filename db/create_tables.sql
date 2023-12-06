b4273ba508dd488d1509c2d1a735334e111271a1
nathan <nathan.poncet.pro@gmail.com>
2023-12-06 10:08:51 +0100

    🔧 chore(.env.example): update database environment variables with default values and add DB_NAME variable for improved configuration 🔧 chore(compose.db.yml): update MYSQL_DATABASE value to use the new DB_NAME variable for consistency 🔥 refactor(python_api/main.py): remove unused Python API file 🔧 chore(db/create_tables.sql): update SQL script to drop and create tables in the new iterator-db schema and insert initial data

--- db/create_tables.sql
+++ db/create_tables.sql
@@ -1,1 +1,11 @@
-use "devops";
+drop schema if exists `iterator-db`;
+
+create schema if not exists `iterator-db`;
+
+use `iterator-db`;
+
+drop table if exists interator;
+
+create table interator (state int DEFAULT 0);
+
+insert into interator (state) values (0);
